6cbb7e5893322f81b526bb838501a38e3f6373f7
Eugene <66390885+kslive@users.noreply.github.com>
2021-01-30 15:52:18 +0300

    Initial Commit

---
+++ lesson1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lesson1
+//
+//  Created by Eugene Kiselev on 30.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
